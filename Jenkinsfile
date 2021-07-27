2917286f5fabdf9e47b46a8600ef66781a46bcfc
livelace <o.popov@livelace.ru>
2021-07-27 06:07:06 +0300

    README.md

---
+++ Jenkinsfile
@@ -0,0 +1,16 @@
+properties([
+        parameters([
+                [
+                        $class: 'CascadeChoiceParameter',
+                        choiceType: 'PT_SINGLE_SELECT',
+                        description: null,
+                        filterLength: 1,
+                        filterable: false,
+                        name: 'OPENNLP_VERSION',
+                        randomName: 'choice-parameter-417264169992283',
+                        referencedParameters: '',
+                        script: [$class: 'ScriptlerScript', parameters: [], scriptlerScriptId: 'opennlp_version.groovy']
+                ]
+        ])])
+
+//k8s_build()
--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,26 +1,21 @@
-jte {
-    pipeline_template = 'k8s_build.groovy'
-}
-
 libraries {
     git {
-        repo_url = 'https://github.com/livelace/digator-opennlp.git'
+        repo_url = "https://github.com/livelace/digator-opennlp.git"
+    }
+    k8s {
+        image = "harbor-core.k8s-2.livelace.ru/dev/jvm:latest"
     }
     kaniko {
-        destination = 'data/digator-opennlp:master-1.8.4'
-        options = '--build-arg OPENNLP_VERSION=1.8.4'
+        destination = "data/digator-opennlp:master-${env.OPENNLP_VERSION}"
+        options = "--build-arg OPENNLP_VERSION=${env.OPENNLP_VERSION}"
     }
     maven {
-        options = '-Dquarkus.package.type=uber-jar -Dopennlp.version=1.8.4'
+        options = "-Dquarkus.package.type=uber-jar -Dopennlp.version=${env.OPENNLP_VERSION}"
     }
     mattermost
     nexus {
-        source = 'target/digator-opennlp-1.0-SNAPSHOT-runner.jar'
-        destination = 'dists-internal/digator-opennlp/digator-opennlp-master-1.8.4.jar'
+        source = "target/digator-opennlp-1.0-SNAPSHOT-runner.jar"
+        destination = "dists-internal/digator-opennlp/digator-opennlp-master-${env.OPENNLP_VERSION}.jar"
     }
-    version
 }
 
-keywords {
-    build_image = 'harbor-core.k8s-2.livelace.ru/dev/jvm:latest'
-}
